a9136c47104d544924c6dd7e732f4aa28f88bed2
Julio Cesar Martinez <juliocesarmartinez1344@gmail.com>
2024-03-12 12:10:21 -0500

    bug fixed

--- src/api/getUserWithEmail.js
+++ src/api/getUserWithEmail.js
@@ -1,6 +1,6 @@
 export const getUserWithEmail = async (email) => {
   try {
-    const response = await fetch(`https://task-api-tau.vercel.app/${email}`);
+    const response = await fetch(`https://task-api-tau.vercel.app/user/${email}`);
     if (!response.ok) {
       throw new Error(`Server responded with status: ${response.status}`);
     }
--- src/api/useCreateUser.js
+++ src/api/useCreateUser.js
@@ -2,8 +2,7 @@ import { Auth } from "@/context/authContext";
 import { useContext } from "react";
 
 export const useCreateUser = () => {
-    
-  const  setCurrentUserDb  = useContext(Auth);
+  const setCurrentUserDb = useContext(Auth);
   const createUserFn = (data) => {
     console.log(data, "Data enviada a backend");
 
@@ -14,12 +13,11 @@ export const useCreateUser = () => {
       },
       body: JSON.stringify(data),
     })
-      .then((response) => {
+      .then(async (response) => {
         if (response.ok) {
-          return response.json().then((user) => {
-            setCurrentUserDb(user);
-            console.log(user, "Usuario creado con éxito");
-          });
+          const user = await response.json();
+          setCurrentUserDb(user);
+          console.log(user, "Usuario creado con éxito");
         } else {
           console.error("Error en la respuesta del servidor", response.status);
           throw new Error(`Error en la solicitud: ${response.statusText}`);
